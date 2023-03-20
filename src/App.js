4ae39196d1b3368bee9c0c1a482d4fb09a7d82ba
Yu Jung Eun <0hh.0553@gmail.com>
2023-03-20 14:08:20 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
-import MyComponent from './MyComponent';
-
+// App.js
+import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
-}
-
+return <MyComponent name="React" favoriteNumber={7}>리액트 </MyComponent>;
+// 콘솔창에 오류메시지가 나타난다.
+};
 export default App;
