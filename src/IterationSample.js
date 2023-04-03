28fd76e7f732d36079b2dde9780f7ac309fb9caa
Yu Jung Eun <0hh.0553@gmail.com>
2023-04-03 14:28:25 +0900

    리스트 추가

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,10 +11,30 @@ const IterationSample = () => {
 
     const [inputText, setInputText] = useState("");
     const [nextId, setNextId] = useState(5);
+    // 새로운 항목을 추가하기 위해서 id 사용
+    
+    const onChange = (e) => setInputText(e.target.value);
+
+    const onClick = // eslint-disable-line no-unused-vars
+        () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText,
+        });
+        setNextId(nextId + 1); // nextId에 1을 더해준다.
+        setNames(nextNames); // names 값을 업데이트 한다.
+        setInputText(""); // inputText를 비운다.
+    };
 
     const nameList = names.map((name)=> <li key={name.id}>{name.text}</li>)
 
-    return <ul>{nameList}</ul>
-}
+    return (
+        <>
+        <input value={inputText} onChange={onChange} />
+        <button onClick={onClick}>추가</button>
+        <ul>{nameList}</ul>
+        </>
+    );
+};
 
 export default IterationSample;
